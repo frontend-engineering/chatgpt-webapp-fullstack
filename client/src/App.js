ebd06344dfb72947edfefa59ac17ffe892afcb25
wade_jhao <wadechaotaiwan@gmail.com>
2023-04-01 17:05:07 +0800

    feat: edit and add prompt feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Chat from './Chat';
-
+import AddPrompt from './CreatePrompt'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Chat />
+      <Router>
+        <Routes>
+          <Route path="/build" element={<Chat />} />
+          <Route path="/addPrompt/" element={<AddPrompt />} />
+          <Route path="/editPrompt/:value" element={<AddPrompt />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
