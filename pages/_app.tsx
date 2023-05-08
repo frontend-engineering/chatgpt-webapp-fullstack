a2f1a926b957fe49b8844a6110bc9b2b468569b2
walkthunder Cooper <walkthunder@163.com>
2023-05-08 14:10:32 +0800

    feat(ads): Add ads script.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
+import Script from 'next/script'
 import "../styles/globals.css";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3614870144525266" crossOrigin="anonymous"></Script>
       <Component {...pageProps} />
       <Analytics />
     </>
