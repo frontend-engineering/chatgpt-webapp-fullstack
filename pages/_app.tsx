d23a995fab3a04fb17cb51117eded2c1f4eefaea
walkthunder Cooper <walkthunder@163.com>
2023-05-16 09:54:18 +0800

    debug(vconsole): Add show secret.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import Script from 'next/script'
+import { useRouter } from 'next/router'
 import "../styles/globals.css";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
   return (
     <>
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3614870144525266" crossOrigin="anonymous"></Script>
@@ -12,9 +14,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         strategy="lazyOnload"
         onLoad={() => {
           if (typeof window !== 'undefined') {
-            console.log('Script has loaded');
+            console.log('Script has loaded', router.asPath)
             setTimeout(() => {
-              new (window as any).VConsole();
+              if (router.asPath.indexOf('showLog=magicfwx') > -1) {
+                new (window as any).VConsole();
+              }
             }, 300)
           }
         }}
